b772ba8893794aa24192b815cede68f105867b4d
Aya <pyon220@gmail.com>
2023-04-07 21:24:24 -0700

    filter function fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 import HomeNavbar from "./components/Navbar/Navbar";
 import Categories from "./components/Categories/Categories";
 import ApiCards from "./components/ApiCards/ApiCards";
 
-// const CategoryContext = createContext(category);
 function App() {
   const [category, setCategory] = useState("");
+  const [apiList, setApiList] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+    const fetchApiLists = async () => {
+      try {
+        const response = await axios.get(`https://api.publicapis.org/entries`);
+        const data = response.data;
+        setApiList(data.entries);
+        setFiltered(data.entries);
+      } catch (error) {
+        console.log(`Error while fetching: ${error}`);
+      }
+    };
+    fetchApiLists();
+  }, []);
 
   return (
     <>
       <HomeNavbar />
       <Categories setCategory={setCategory} />
-      <ApiCards category={category} />
+      <ApiCards category={category} apiList={apiList} filtered={filtered} />
     </>
   );
 }
--- src/components/ApiCards/ApiCards.jsx
+++ src/components/ApiCards/ApiCards.jsx
@@ -7,57 +7,52 @@ import "./ApiCards.css";
 import { CardGroup } from "react-bootstrap";
 
 function ApiCards(props) {
-  const [apiList, setApiList] = useState([]);
-  const [filtered, setFiltered] = useState([]);
+  const [apiList, setApiList] = useState(props.apiList);
+  const [filtered, setFiltered] = useState(props.apiList);
   const [category, setCategory] = useState("");
-  // setCategory(props.category);
 
   useEffect(() => {
-    const fetchApiLists = async () => {
-      try {
-        const response = await axios.get(`https://api.publicapis.org/entries`);
-        const data = response.data;
-        setApiList(data.entries);
-        setFiltered(data.entries);
-      } catch (error) {
-        console.log(`Error while fetching: ${error}`);
-      }
-    };
-    fetchApiLists();
-    setCategory(props.category);
+    setApiList(props.apiList);
+    setFiltered(props.apiList);
+    console.log("---------useEffect---------");
+    console.log(apiList);
+    console.log(filtered);
   }, []);
 
   const apiCards = filtered.map((card, key) => (
     <>
-      <Card className="card--container">
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item key={key}>
-            {card.Category.toUpperCase()}
-          </ListGroup.Item>
-        </ListGroup>{" "}
-        <Card.Body>
-          <Card.Title>{card.API}</Card.Title>
-          <Card.Text>{card.Description}</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>
-            Auth type : {card.Auth ? card.Auth : "---"}
-          </ListGroup.Item>
-          {/* <ListGroup.Item>HTTPS : {Card.HTTPS}</ListGroup.Item> */}
-          <ListGroup.Item>Cors : {card.Cors.toUpperCase()}</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Card.Link target="_blank" href={card.Link}>
-            API Link
-          </Card.Link>
-        </Card.Body>
-      </Card>
+      <a className="listCard--body" target="_blank" href={card.Link}>
+        <Card className="card--container">
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item key={key}>
+              {card.Category.toUpperCase()}
+            </ListGroup.Item>
+          </ListGroup>{" "}
+          <Card.Body>
+            <Card.Title>{card.API}</Card.Title>
+            <Card.Text>{card.Description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>
+              Auth type : {card.Auth ? card.Auth : "---"}
+            </ListGroup.Item>
+            {/* <ListGroup.Item>HTTPS : {Card.HTTPS}</ListGroup.Item> */}
+            <ListGroup.Item>Cors : {card.Cors.toUpperCase()}</ListGroup.Item>
+          </ListGroup>
+          {/* <Card.Body>
+            <Card.Link target="_blank" href={card.Link}>
+              API Link
+            </Card.Link>
+          </Card.Body> */}
+        </Card>
+      </a>
     </>
   ));
 
   useEffect(() => {
     if (props.category === "All") {
-      setFiltered(apiList);
+      setApiList(props.apiList);
+      setFiltered(props.apiList);
       return;
     }
     const filtered = apiList.filter((card) =>
@@ -68,8 +63,9 @@ function ApiCards(props) {
 
   return (
     <>
+      {/* <p>Current Category: {apiList[1].Category}</p> */}
       <p>Current Category: {props.category}</p>
-      <p>Number of List: {apiCards.length}</p>
+      <p>Number of API: {apiCards.length}</p>
       <div className="cardList--fieldset">{apiCards}</div>
     </>
   );
